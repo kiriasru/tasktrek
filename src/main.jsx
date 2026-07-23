ccf2ae880c8ddb409e3847f96e28831eb98a5cda
kiriasru <kiriasru@gmail.com>
2026-07-22 18:36:38 -0600

    feat: add initial app layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Card from './components/Card.jsx'
-import Todo from './components/Todo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Card />
-    <Todo />
+    <App />
   </StrictMode>,
 )
